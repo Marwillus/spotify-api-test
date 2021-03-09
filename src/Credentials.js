5317f622fc359b680908e1198e6b4c21ba196267
marcus <marcusxwill@gmail.com>
2021-03-09 15:29:17 +0100

    basis funktioniert

---
+++ src/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+  return {
+    ClientId: "7c6470bf925d46cd9665f84e26f18e74",
+    ClientSecret: "660f6cd47aa04a74bb9c9048c9c059c5",
+  };
+};
+
+export { Credentials };
